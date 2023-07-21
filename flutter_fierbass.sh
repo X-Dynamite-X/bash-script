c5f89512615a93144c56445c55a6742c6c2702c7
Dynamite <abdmoklss@gmail.com>
2023-07-21 21:23:50 +0300

    Add files via upload

---
+++ flutter_fierbass.sh
@@ -0,0 +1,5 @@
+export PATH="$PATH:/home/dynamite/flutter/bin"                                                                                                                       127 ⨯
+flutter pub add firebase_auth
+flutter pub add firebase_core 
+flutter pub add firebase_storage
+ flutter pub add cloud_firestore
---
+++ setup_install.sh
@@ -0,0 +1,24 @@
+
+sudo apt update && sudo apt -y full-upgrade
+echo "Done.. 1"
+sudo apt update 
+echo "Done.. 2"
+sudo apt upgrade 
+echo "Done.. 3"
+sudo apt install kali-linux-*
+echo "Done.. 4"
+sudo apt install kali-tools-*
+echo "Done.. 5"
+sudo apt install -y flatpak
+echo "Done.. 6"
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo "Done.. 7"
+sudo apt install gnome-software-plugin-flatpak
+echo "Done.. 8"
+sudo apt install qbittorrent
+echo "Done.. 9"
+sudo apt-get update && sudo apt-get install spotify-client
+sudo apt update && sudo apt upgrade
+echo "Done.. 10"
+
+
